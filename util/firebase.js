39aebbde9396660f63496dcb11c21f2d2aab3018
Amna <ajaldh26x@gmail.com>
2021-01-26 10:13:45 +0300

    changes

--- util/firebase.js
+++ util/firebase.js
@@ -1,9 +1,16 @@
 const firebase = require("firebase");
 
- const data = process.env.KEY_INITIALIZE;
- const jsoninitializeApp = JSON.parse(data);
+ 
 
-const firebaseApp = firebase.initializeApp(jsoninitializeApp);
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.KEY_INITIALIZE,
+  authDomain: "mosiqa-player.firebaseapp.com",
+  projectId: "mosiqa-player",
+  storageBucket: "mosiqa-player.appspot.com",
+  messagingSenderId: process.env.MSG_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MSMNT_ID,
+});
 
 
  const db = firebaseApp.firestore();
